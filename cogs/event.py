24df64a51832002782b6f630ba9f9bbeccc1fa59
KIMI <plusee@naver.com>
2022-07-23 19:46:18 +0900

    1.3v 이벤트 시간 설정

--- cogs/event.py
+++ cogs/event.py
@@ -32,28 +32,36 @@ class 이벤트(commands.Cog):
     @commands.has_role("mods")
     @cooldown(1, 2, BucketType.user)
     @is_channel(db.channel_data["관리자"])
-    async def 이벤트(self, ctx, name: str):
+    async def 이벤트(self, ctx, name: str, time):
         """ 이벤트를 발생시킵니다. (관리자용) (!이벤트 [이벤트명]) """
         try:
             if name == "지진":
+                if time > 60:
+                    await ctx.send("지진 이벤트 시간은 10 분 이내로 설정해주세요!")
+                if time == None:
+                    time = 5
                 event.rob_event = True
                 msg_channel = ctx.guild.get_channel(db.channel_data["주막"])
-                await msg_channel.send(f"{ctx.guild.get_role(973444672754171944).mention} 지진이 발생했습니다! 지진이 발생했습니다!!")
-                await asyncio.sleep(60)
+                await msg_channel.send(f"{ctx.guild.get_role(922275420764770334).mention} 지진이 발생했습니다! 지진이 발생했습니다!!")
+                await asyncio.sleep(60 * time)
                 event.rob_event = False
-                await msg_channel.send(f"{ctx.guild.get_role(973444672754171944).mention} 지진이 멈췄습니다!")
+                await msg_channel.send(f"{ctx.guild.get_role(922275420764770334).mention} 지진이 멈췄습니다!")
             elif name == "보스":
+                if time > 60:
+                    await ctx.send("보스 이벤트 시간은 60 분 이내로 설정해주세요!")
+                if time == None:
+                    time = 30
                 if event.isboss:
                     await ctx.send("현재 보스레이드 중 입니다.")
                     return
                 event.isboss = True
                 event.boss = boss_json['빌런왕']
                 msg_channel = ctx.guild.get_channel(db.channel_data["사냥터"])
-                await msg_channel.send(f"{ctx.guild.get_role(973444672754171944).mention} 보스가 등장 했습니다. 보스가 등장했습니다!! 보스 이름 {event.boss['name']}")
-                await asyncio.sleep(60)
+                await msg_channel.send(f"{ctx.guild.get_role(922275420764770334).mention} 보스가 등장 했습니다. 보스가 등장했습니다!! 보스 이름 {event.boss['name']}")
+                await asyncio.sleep(60 * time)
                 event.isboss = False
                 event.boss = ''
-                await msg_channel.send(f"{ctx.guild.get_role(973444672754171944).mention} 보스 사냥을 완료 하였습니다. 모든 기여도 정산이 완료 되었습니다.")
+                await msg_channel.send(f"{ctx.guild.get_role(922275420764770334).mention} 보스 사냥을 완료 하였습니다. 모든 기여도 정산이 완료 되었습니다.")
             else:
                 await ctx.send('없는 명령어 입니다.')
 
